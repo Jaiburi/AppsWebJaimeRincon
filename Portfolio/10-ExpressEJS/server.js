ac7d91d6b901822b9bb258e93f5762d13b8bb34a
Jaime Rincon <0260590@up.edu.mx>
2024-10-30 13:07:55 -0600

    Portfolio 10

--- Portfolio/10-ExpressEJS/server.js
+++ Portfolio/10-ExpressEJS/server.js
@@ -2,18 +2,74 @@ const express = require("express");
 const app = express();
 const https = require("https");
 
-// TODO: configure the express server
+// Configure express server
+app.set('view engine', 'ejs'); // Set EJS as templating engine
+app.use(express.static('public')); // Serve static files
+app.use(express.json()); // Handle JSON input/output
+app.use(express.urlencoded({ extended: true })); // Handle form data
 
-const longContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const longContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare...";
 
 let posts = [];
 let name;
 
+// Root route
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/html/index.html");
+    res.render("index"); // Changed to render index.ejs instead of sending HTML file
+});
+
+// Unsecured login route (GET)
+app.get("/login", (req, res) => {
+    name = req.query.name;
+    res.render("test", { 
+        name: name,
+        securityLevel: "unsecured"
+    });
+});
+
+// Secured login route (POST)
+app.post("/login", (req, res) => {
+    name = req.body.name;
+    res.render("test", { 
+        name: name,
+        securityLevel: "secured"
+    });
+});
+
+// Home route (blog listing)
+app.get("/home", (req, res) => {
+    if (!name) {
+        res.redirect("/");
+    } else {
+        res.render("home", { name: name, posts: posts });
+    }
+});
+
+// Create new post
+app.post("/post", (req, res) => {
+    const { title, content } = req.body;
+    posts.push({ 
+        id: posts.length + 1,
+        title, 
+        content 
+    });
+    res.redirect("/home");
+});
+
+// View single post
+app.get("/post/:id", (req, res) => {
+    const post = posts.find(p => p.id === parseInt(req.params.id));
+    res.render("post", { name: name, post: post });
+});
+
+app.post("/post/:id/delete", (req, res) => {
+  const postId = parseInt(req.params.id);
+  // Filter out the post with the matching id
+  posts = posts.filter(post => post.id !== postId);
+  // Redirect back to the home page after deletion
+  res.redirect('/home');
 });
 
 app.listen(3000, (err) => {
-  console.log("Listening on port 3000");
+    console.log("Listening on port 3000");
 });
